7d2806304cbbe8b39e35d44a1028318dd91bd838
Samyak S Sarnayak <samyak201@gmail.com>
2020-07-27 22:52:44 +0530

    [BASIC] added a basic structure of the project

---
+++ fourier-dnn/ffm.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+class BasicFFM(tf.keras.layers.Layer):
+    def __init__(self):
+        raise NotImplementedError
+
+class GaussianFFM(tf.keras.layers.Layer):
+    def __init__(self):
+        raise NotImplementedError
