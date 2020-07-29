fa288bb25aa6be47bb04f6cc07b23376ce5d7803
Samyak S Sarnayak <samyak201@gmail.com>
2020-07-29 11:37:33 +0530

    [CI] moved tests to /tests and added pytest to workflow

--- tests/test_ffm_layers.py
+++ tests/test_ffm_layers.py
@@ -1,18 +1,17 @@
-import numpy as np
 import pytest
 import tensorflow as tf
-from ffm import *
+from fourier_dnn.ffm import BasicFFM, GaussianFFM
 
 # pylint: disable=no-value-for-parameter, unexpected-keyword-arg, arguments-differ
 # pylint: disable=attribute-defined-outside-init
 
-def test_basic_FFM():
+def test_BasicFFM():
 
-    """ A simple test that checks the output of the Basic FFM layer """ 
+    """ A simple test that checks the output of the Basic FFM layer """
 
     # Initializing random input
     tf.random.set_seed(1)
-    test_input = tf.random.uniform(shape = [500, 2])
+    test_input = tf.random.uniform(shape=[500, 2])
 
     # Creating the layer and appling FFM
     basic_FFM_kernel = BasicFFM()
@@ -20,15 +19,15 @@ def test_basic_FFM():
 
     assert output.shape == (500, 4)
 
-def test_gaussian_FFM():
+def test_GaussianFFM():
 
     """ A simple test that checks the output of the Gaussian FFM Layer """
     # Initializing random input
     tf.random.set_seed(1)
-    test_input = tf.random.uniform(shape = [500, 2])
+    test_input = tf.random.uniform(shape=[500, 2])
 
     # Creating the layer and appling FFM
-    gaussian_FFM_kernel = GaussianFFM(num_units = 126, std_dev = 2.3)
+    gaussian_FFM_kernel = GaussianFFM(num_units=126, std_dev=2.3)
     output = gaussian_FFM_kernel(test_input)
 
     assert output.shape == (500, 252)
